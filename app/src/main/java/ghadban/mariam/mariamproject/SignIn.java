5ec2b7c88aff834c949646e570a267cb38a1e74e
mariamgh1 <ghadbanmariam@gmail.com>
2020-10-14 18:33:20 +0300

    i build a new project

---
+++ app/src/main/java/ghadban/mariam/mariamproject/SignIn.java
@@ -0,0 +1,34 @@
+package ghadban.mariam.mariamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignIn extends AppCompatActivity {
+
+    private EditText etInEmail, etInPassword;
+    private Button Loginbtn, Signupbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        etInEmail = findViewById(R.id.etInEmail);
+        etInPassword = findViewById(R.id.etInPassword);
+        Loginbtn = findViewById(R.id.Loginbtn);
+        Signupbtn = findViewById(R.id.Signupbtn);
+    }
+
+    private void checkForm()//hl 3bahn sa7(sign up)
+    {
+        String Email = etInEmail.getText().toString();
+        String Password = etInPassword.getText().toString();
+        String Login = Loginbtn.getText().toString();
+        String Signup = Signupbtn.getText().toString();
+
+
+    }
+}
--- app/src/main/java/ghadban/mariam/mariamproject/SignUp.java
+++ app/src/main/java/ghadban/mariam/mariamproject/SignUp.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
-    private EditText etFirstName,etLastName,etPassword,etVrPassword,etEmail,etPhone;
+    private EditText etFirstName, etLastName, etPassword, etVrPassword, etEmail, etPhone;
     private Button Savebtn;
 
     @Override
@@ -15,38 +15,37 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        etFirstName=findViewById(R.id.etFirstName);
-        etLastName=findViewById(R.id.etLastName);
-        etPassword=findViewById(R.id.etPassword);
-        etVrPassword=findViewById(R.id.etVrPassword);
-        etEmail=findViewById(R.id.etEmail);
-        etPhone=findViewById(R.id.etPhone);
-        Savebtn=findViewById(R.id.Savebtn);
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        etPassword = findViewById(R.id.etPassword);
+        etVrPassword = findViewById(R.id.etVrPassword);
+        etEmail = findViewById(R.id.etEmail);
+        etPhone = findViewById(R.id.etPhone);
+        Savebtn = findViewById(R.id.Savebtn);
     }
 
     private void checkForm()//hl 3bahn sa7(sign up)
     {
-        String firstn=etFirstName.getText().toString();
-        String lastn=etLastName.getText().toString();
-        String pass=etPassword.getText().toString();
-        String verPass=etVrPassword.getText().toString();
-        String email=etEmail.getText().toString();
-        String phone=etPhone.getText().toString();
-        String save=Savebtn.getText().toString();
+        String firstn = etFirstName.getText().toString();
+        String lastn = etLastName.getText().toString();
+        String pass = etPassword.getText().toString();
+        String verPass = etVrPassword.getText().toString();
+        String email = etEmail.getText().toString();
+        String phone = etPhone.getText().toString();
+        String save = Savebtn.getText().toString();
 
-        boolean isOK=true;
+        boolean isOK = true;
 
-        if(firstn.length()<2)
-        {
-            isOK=false;
+        if (firstn.length() < 2) {
+            isOK = false;
             etFirstName.setError("at least two letter");
         }
 
-        if (email.length()<5 || (email.indexOf('@')==0 ) || email.indexOf('@')>=email.length()-2 || email.indexOf('.')==0
-                ||email.indexOf('.')>=email.length()-1 ||email.lastIndexOf('.')<email.indexOf('@'))
-        {
-            isOK=false;
+        if (email.length() < 5 || (email.indexOf('@') == 0) || email.indexOf('@') >= email.length() - 2 || email.indexOf('.') == 0
+                || email.indexOf('.') >= email.length() - 1 || email.lastIndexOf('.') < email.indexOf('@')) {
+            isOK = false;
             etEmail.setError("Wrong E-mail. Try again");
         }
 
     }
+}
